9e99a572563c49302bd8faee2868d723817cddc9
xiaocong <1561331574@qq.com>
2025-06-09 19:01:03 +0800

    整理文件结构

--- task/Strategy/header/constant.h
+++
@@ -1,4 +0,0 @@
-extern const double M_PI = 3.14159265358979323846;
-extern const double Kp = 2.2;
-extern const short tick_delay = 4;
-
--- task/Strategy/header/globalVariable.h
+++
@@ -1,4 +0,0 @@
-/*用以存储一些全局变量*/
-extern int tick;
-extern double lastBallx, lastBally;	// 记录上一拍球的位置
-extern int tickBeginPental;	// 记录点球开始时间
